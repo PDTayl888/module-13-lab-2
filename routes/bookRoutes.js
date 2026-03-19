791750e4ead81ec6504161752a629317906ee9af
Patrick D. Taylor <patrickdtaylor1980@gmail.com>
2026-03-19 18:34:48 -0400

    edited bookRoutes

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -1,42 +1,57 @@
-const express = require('express');
+const express = require("express");
+const Book = require("../models/Book");
 const router = express.Router();
 
-router.post('/', async (req, res) => {
-    try {
-
-    } catch (error) {
-
-    }
+router.post("/", async (req, res) => {
+  try {
+    const newBook = new Book(req.body);
+    const savedBook = await newBook.save();
+    res.status(201).json(savedBook);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 });
 
-router.get('/', async (req, res) => {
-    try {
-
-    } catch (error) {
-
-    }
+router.get("/", async (req, res) => {
+  try {
+    const books = await Book.find();
+    res.json(books);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 });
 
-router.get('/', async (req, res) => {
-    try {
-
-    } catch (error) {
-
-    }
+router.get("/:id", async (req, res) => {
+  try {
+    const book = await Book.findById(req.params.id);
+    if (!book) return res.status(404).json({ message: "BOOK NOT FOUND" });
+    res.json(book);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
-router.put('/', async (req, res) => {
-    try {
-
-    } catch (error) {
-
-    }
+router.put("/:id", async (req, res) => {
+  try {
+    const updatedBook = await Book.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!updatedBook) return res.status(404).json({message: 'BOOK NOT FOUND'});
+    res.json(400).json({message: error.message});
+  } catch (error) {
+    res.status(400).json({message: error.message});
+  }
 });
 
-router.delete('/', async (req, res) => {
-    try {
-
-    } catch (error) {
-
-    }
+router.delete("/:id", async (req, res) => {
+  try {
+    const deletedBook = await Book.findByIdAndDelete(req.params.id);
+    if (!deletedBook) return res.status(404).json({ message: "BOOK NOT FOUND"});
+    res.json({message: 'BOOK DELETED SUCCESSFULLY'});
+  } catch (error) {
+    res.status(500).json({message: error.message});
+  }
 });
+
+module.exports = router;
