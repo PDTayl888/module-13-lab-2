bc3fb7c9a82a53356f4862db1c0172e8b94e66be
Patrick D. Taylor <patrickdtaylor1980@gmail.com>
2026-03-19 15:43:19 -0400

    edited server

--- server.js
+++ server.js
@@ -2,3 +2,15 @@ const connectDB = require('./db/connection');
 
 const express = require('express');
 require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+connectDB();
+
+app.use(express.json());
+
+app.use('/api/books', bookRoutes);
+
+app.listen(PORT, () => {
+    console.log(`SERVER RUNNING AT PORT ${PORT}`);
+});
