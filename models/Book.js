549898ff9e4fd706a73a65b75a6618c8eec69b94
Patrick D. Taylor <patrickdtaylor1980@gmail.com>
2026-03-19 15:28:50 -0400

    edited Book schema

--- models/Book.js
+++ models/Book.js
@@ -1,16 +1,23 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const bookSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: [true, 'Title required']
-    },
-    author: {
-        type: String,
-        required: [true, 'Author required']
-    }, 
-    isbn: {
-        type: String,
-        unique: true
-    }
-})
+  title: {
+    type: String,
+    required: [true, "Title required"],
+  },
+  author: {
+    type: String,
+    required: [true, "Author required"],
+  },
+  isbn: {
+    type: String,
+    unique: true,
+  },
+  publishedDate: {
+    type: Date,
+  },
+  inStock: {
+    type: Boolean,
+    default: true,
+  },
+});
